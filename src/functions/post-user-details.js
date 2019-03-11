14f2ab0215c1cabecda16a0b53b71720f689e4ab
futuregerald <gerald.onyango@gmail.com>
2019-03-11 16:56:46 -0400

    testing

--- src/functions/post-user-details.js
+++ src/functions/post-user-details.js
@@ -4,16 +4,11 @@ const timestamp = require('mongoose-timestamp');
 let conn = null;
 
 exports.handler = async function(event, context, callback) {
+  const uri = process.env.MONGO_URL;
   console.log('trying');
   context.callbackWaitsForEmptyEventLoop = false;
   // const body = JSON.parse(event.body);
   // console.log(body);
-
-  try {
-    conn = await mongoose.connect(uri);
-  } catch (error) {
-    console.log(error);
-  }
 
   const PersonSchema = new mongoose.Schema({
     Email: {
@@ -35,15 +30,20 @@ exports.handler = async function(event, context, callback) {
       required: false,
     },
   });
+  /*
+  try {
+    conn = await mongoose.connect(uri);
+  } catch (error) {
+    console.log(error);
+  }
+  */
   PersonSchema.plugin(timestamp);
-  const User = mongoose.model('User', PersonSchema);
-  const uri = process.env.MONGO_URL;
+  // const User = mongoose.model('User', PersonSchema);
+
+  /*
+
   console.log('trying to connect');
-  const user = new User({
-    Email: 'testing_from_function@tested.com',
-    Address: '123 fake st. New York City, NY 10023',
-    IdentityID: '798f2522-aab3-4e01-84ee-8331f3b83e6b',
-  });
+  
   const newUser = await user.save();
   console.log(newUser);
 
@@ -51,6 +51,33 @@ exports.handler = async function(event, context, callback) {
 
   const doc = await User.findOne({ email: 'testing_from_function@tested.com' });
   console.log(doc);
+  */
+  if (conn == null) {
+    conn = await mongoose.createConnection(uri, {
+      // Buffering means mongoose will queue up operations if it gets
+      // disconnected from MongoDB and send them when it reconnects.
+      // With serverless, better to fail fast if not connected.
+      bufferCommands: false, // Disable mongoose buffering
+      bufferMaxEntries: 0, // and MongoDB driver buffering
+    });
+    conn.model('User', PersonSchema);
+  }
 
-  return doc;
+  const UserModel = conn.model('User');
+  const user = new UserModel({
+    Email: 'testing2@tested.com',
+    Address: '123 fake st. New York City, NY 10023',
+    IdentityID: '798f2522-aab3-4e01-84ee-8331f3b83e6b',
+    UseNewUrlParser: true,
+  });
+  const newUser = await user.save();
+  console.log(newUser);
+
+  const doc = await UserModel.findOne({ Email: 'testing2@tested.com' });
+  console.log(doc);
+
+  return callback(null, {
+    statusCode: 200,
+    body: doc,
+  });
 };
